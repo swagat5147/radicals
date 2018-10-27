77db243fcc20cd2bec7bcdd85f4c9ca67f95ce9e
swagat5147 <swagatparija.1@gmail.com>
2018-10-27 23:09:25 +0530

    added changes
    jquery added locally.
    modify in popup.js.
    modify in style.css

--- popup.js
+++ popup.js
@@ -1,25 +1,15 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-'use strict';
 
-let changeColor = document.getElementById('changeColor');
+var outUrl;
+var url = document.getElementById("url");
+// first get the windowid
+chrome.windows.getCurrent(function(window) {
+    // then get the current active tab in that window
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+   function(tabs){
+      url.value= tabs[0].url;
+   }
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+);
+
 });
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
-// var pathname = window.location.href;
-// var url = document.getElementById("url");
-// url.innerHTML = pathname;
-
